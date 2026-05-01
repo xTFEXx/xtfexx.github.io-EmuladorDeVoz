c88be53d1cbd614a7b1b20652842db10bd3f42bd
xTFEXx <facundotrujillo08@gmail.com>
2026-04-30 23:43:46 -0300

    v0.0.5

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'walkie-fx-cache-v0.0.6';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data === 'SKIP_WAITING') self.skipWaiting();
+});
